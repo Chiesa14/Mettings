8166da6b45ed3f7765cff7f457598a320912c33b
Chiesa14 <remychiesa14@gmail.com>
2023-10-28 20:23:36 +0200

    Setting tailwin css

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Home } from "./Screens";
+import { Text, View } from "react-native";
+import { NativeWindStyleSheet } from "nativewind";
+
+NativeWindStyleSheet.setOutput({
+  default: "native",
+});
 
 const stack = createNativeStackNavigator();
 
@@ -7,7 +14,11 @@ export default function App() {
   return (
     <NavigationContainer>
       <stack.Navigator>
-        <stack.Screen />
+        <stack.Screen
+          name="Home"
+          component={Home}
+          options={{ headerShown: false }}
+        />
       </stack.Navigator>
     </NavigationContainer>
   );
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["./App.js", "./Screens/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
