4e27a484e33fe184ce7d50671576ab8ff926a5cc
Chiesa14 <remychiesa14@gmail.com>
2023-10-29 19:21:28 +0200

    Trying  agora

--- Screens/Home.jsx
+++ Screens/Home.jsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Text } from "react-native";
 
 const Home = () => {
   return (
     <SafeAreaView>
-      <Text>Home</Text>
+      <Text>CHiesa</Text>
     </SafeAreaView>
   );
 };
